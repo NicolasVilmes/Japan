e21482271cb4f8341e9129d2bfc1cd5ec982ede5
NicolasVilmes <nicolas_vilmes@estudante.sesisenai.org.br>
2022-10-10 00:32:46 -0300

    inset nav scrolling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+// Write Machine //
+
 const element = document.querySelector("h2");
 
 function digitatexto(element) {
@@ -9,3 +11,24 @@ function digitatexto(element) {
 }
 
 setTimeout(digitatexto(element), 5000);
+
+// Nav Scroll //
+
+window.onscroll = scroll;
+
+function scroll() {
+  var scrollTop = window.pageYOffset;
+  if (scrollTop > 30) {
+    try {
+      document.getElementById("firstnav").id = "secondnav";
+    } catch (e) {
+      false;
+    }
+  } else {
+    try {
+      document.getElementById("secondnav").id = "firstnav";
+    } catch (e) {
+      false;
+    }
+  }
+}
